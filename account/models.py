19d3bda69eec6168c2100f84a71e41bd49539c61
Minyong <naminyong97@gmail.com>
2021-05-29 00:51:27 +0900

    db 수정

--- account/models.py
+++ account/models.py
@@ -11,8 +11,10 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', default=None)
     nickname = models.CharField(max_length=20, default=None)
     gender = models.CharField(max_length=1,choices=GENDER,default='Male')
+    gender_open = models.BooleanField(default=True)
     major = models.CharField(max_length=20, default=None)
     studentid = models.CharField(max_length=20, default=None)
+    studentid_open = models.BooleanField(default=True)
     introduce = models.TextField(default=None)
     matewant = models.BooleanField(default=False)
     photo = models.ImageField(upload_to="photo", default='media/photo/default_photo.png')
@@ -22,10 +24,10 @@ class Profile(models.Model):
         return self.nickname
 
 class Lecture(models.Model):
-    subject = models.CharField(max_length=30, default=None)
-    subnum = models.CharField(max_length=20, default=None)
-    professor = models.CharField(max_length=20, default=None)
-    subtime = models.CharField(max_length=20, default=None)
+    subject = models.CharField(max_length=100, default=None)
+    subnum = models.CharField(max_length=100, primary_key=True, default=None)
+    professor = models.CharField(max_length=100, blank=True, null=True)
+    subtime = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.subject
