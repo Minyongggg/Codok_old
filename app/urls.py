435f51358e29a51624a0254ce0f21463644bd9b9
Minyong <naminyong97@gmail.com>
2021-05-29 17:00:11 +0900

    community 삭제

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('edit/<int:post_pk>', views.edit, name="edit"),
     path('delete/<int:post_pk>', views.delete, name="delete"),
     path('delete_comment/<int:post_pk>/<int:comment_pk>', views.delete_comment, name="delete_comment"),
+
+    #chatting
+    path('friends/<str:lecture_pk>', views.friends, name="friends"),
 ]
